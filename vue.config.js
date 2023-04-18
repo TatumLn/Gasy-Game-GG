65ed62687c5cac467812a4924a7b1708c98c1d54
TatumLn <linautatum501@gmail.com>
2023-04-18 14:20:26 +0300

    Fanorona Malagasy

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+const publicPath = process.env.NODE_ENV === 'production' ? '/Gasy-Game-GG/' : '/'
+module.exports = {
+ publicPath: publicPath,
+}
